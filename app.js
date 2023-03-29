bfd82c216bdc26c47ded3689d62b1ecb11c299a1
Ahmed Raza <99830227+ahmedraza41190@users.noreply.github.com>
2023-03-29 08:04:32 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// localStorage.setItem('name','Saad Ali')
+// localStorage.setItem('name','Saad Muhammad')
+
+// var a = localStorage.getItem('name')
+// document.write(a)
+
+// localStorage.clear()
+
+// sessionStorage.setItem('name','Huzaifa')
+
+function signup(){
+    var email = document.getElementById('semail').value
+    var pass = document.getElementById('spass').value
+    localStorage.setItem('email',email)
+    localStorage.setItem('pass',pass)
+    alert('Data inserted Successfully')
+    location.href = './signin.html'  //onclick to go on signin page
+ }
+ 
+ function signin(){
+     var email = document.getElementById('lemail').value
+     var pass = document.getElementById('lpass').value
+     var getemail = localStorage.getItem('email')
+     var getpass = localStorage.getItem('pass')
+     if(email === getemail && pass === getpass ){
+         alert('Login Success')
+         location.href = './welcome.html'
+     }
+     else{
+         alert('Jaa beta sign up kar k aa ...')
+         location.href = './signup.html'
+     }
+ }
