a7b724aa8871d2c8bd8514628bc7a67085774e82
Ahmed Raza <99830227+ahmedraza41190@users.noreply.github.com>
2023-03-31 03:13:00 +0500

    Second commit

--- app.js
+++ app.js
@@ -8,26 +8,27 @@
 
 // sessionStorage.setItem('name','Huzaifa')
 
-function signup(){
+function signup() {
     var email = document.getElementById('semail').value
     var pass = document.getElementById('spass').value
-    localStorage.setItem('email',email)
-    localStorage.setItem('pass',pass)
+    localStorage.setItem('email', email)
+    localStorage.setItem('pass', pass)
     alert('Data inserted Successfully')
     location.href = './signin.html'  //onclick to go on signin page
- }
- 
- function signin(){
-     var email = document.getElementById('lemail').value
-     var pass = document.getElementById('lpass').value
-     var getemail = localStorage.getItem('email')
-     var getpass = localStorage.getItem('pass')
-     if(email === getemail && pass === getpass ){
-         alert('Login Success')
-         location.href = './welcome.html'
-     }
-     else{
-         alert('Jaa beta sign up kar k aa ...')
-         location.href = './signup.html'
-     }
- }
+    console.log(email)
+}
+
+function signin() {
+    var email = document.getElementById('lemail').value
+    var pass = document.getElementById('lpass').value
+    var getemail = localStorage.getItem('email')
+    var getpass = localStorage.getItem('pass')
+    if (email === getemail && pass === getpass) {
+        alert('Login Success')
+        location.href = './welcome.html'
+    }
+    else {
+        alert('Please check your email & password and register your self')
+        //  location.href = './signup.html'
+    }
+}
