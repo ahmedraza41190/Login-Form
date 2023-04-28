02022369b9baec56c6abd19bd7907a744c8f75b8
Ahmed Raza <99830227+ahmedraza41190@users.noreply.github.com>
2023-04-28 15:48:44 +0500

    7th commit

--- app.js
+++ app.js
@@ -9,26 +9,37 @@
 // sessionStorage.setItem('name','Huzaifa')
 
 function signup() {
+
     var email = document.getElementById('semail').value
     var pass = document.getElementById('spass').value
+
     localStorage.setItem('email', email)
     localStorage.setItem('pass', pass)
-    alert('Data inserted Successfully')
-    location.href = './index.html'  //onclick to go on signin page
-    console.log(email)
+
+    if (email == '' || pass == '') {
+        alert('Please check your email & password and register your self')
+    }
+    else {
+        alert('Data inserted Successfully')
+        location.href = './index.html'  //onclick to go on signin page
+        console.log(email)
+    }
 }
 
 function signin() {
+
     var email = document.getElementById('lemail').value
     var pass = document.getElementById('lpass').value
+
     var getemail = localStorage.getItem('email')
     var getpass = localStorage.getItem('pass')
-    if (email === getemail && pass === getpass) {
-        alert('Login Success')
-        location.href = './welcome.html'
+
+    if (email == '' || pass == '') {
+        alert('Please check your email & password and register your self')
     }
     else {
-        alert('Please check your email & password and register your self')
-        //  location.href = './signup.html'
+        alert('Data inserted Successfully')
+        location.href = './welcome.html'  //onclick to go on signin page
+        console.log(email)
     }
 }
